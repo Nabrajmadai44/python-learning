53377dcec98ba7480328318fa807017e4ee8fb43
nabrajmadai44 <nabraj0444@gmail.com>
2024-12-17 21:39:59 +0545

    first commit

---
+++ .vs/conditionalstatements.py
@@ -0,0 +1,81 @@
+# if statement
+'''
+syntax
+if (condition):
+    statement
+Rest of code
+'''
+'''
+if 5>2:
+    print("Greater")
+print("rest of the code")
+
+
+
+if 5<2:
+    print('Greater')
+print("Smaller")
+
+
+
+a = int(input("Enter Number greater than 2: "))
+if(a>2):
+    print("You have entered:", a)
+'''   
+   
+   
+# Nested If Statement
+'''Syntax
+if(condition):
+    block of statement
+    if(condition):
+        block of statement
+    if(condition):
+        block of statement
+if(condition):
+    block of statement
+Rest of the code
+'''
+'''
+if 5>2:
+    print("Greater")
+    print("5 is greater than 2")
+    if(6>2):
+        print("6 is greater than 2")
+print("Rest of the code")
+'''
+
+
+
+# If Statement with Logical Operator
+'''
+syntax
+if((conditiion1) and (condition2)):
+    statement
+Rest of the code
+'''
+# if 5>2 and 7>3:
+#     print("True")
+# print("Rest of the code")
+
+
+# if 5>2 or 7<3:
+#     print("True")
+# print("Rest of the code")
+
+
+
+'''
+#If else statement
+syntax
+if(condition):
+    statement1
+else
+    statement2
+Rest of the code
+'''
+if 5<2:
+    print("Greater")
+else:
+    print("smaller")
+
---
+++ Practice/practice.py
@@ -0,0 +1,29 @@
+# #String slicing
+# word = 'Navya'
+# sl=word[1:5]
+# print(sl)
+# sl1 = word[1:4:2]
+# print(sl1)
+# w = "amazing"
+# sll=w[1:6:2]
+# print(sll)
+
+
+# #string functions 
+
+# word = "python hello devil"
+# w=len(word)
+# print(w)
+# w1=word.count("python")
+# print(w1)
+# print(word.capitalize())
+# print(word.endswith("on"))
+# print(word.find("devil"))
+# print(word.replace("hello","language is"))
+
+Name = input("Good Morning ")
+print(Name)
+
+
+
+
